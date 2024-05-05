6c456b33563ae1238e92555b923c35ffa9e4385e
Piny0net <pinyonete@gmail.com>
2024-05-05 13:57:00 +0200

    ssh key command

--- terra.tf
+++ terra.tf
@@ -14,4 +14,19 @@ resource "aws_instance" "nginx-server" {
   }
   vpc_security_group_ids = ["sg-0d46bbb050b90a91a"]
   subnet_id             = "subnet-0339e03548da3f417"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y nginx
+              sudo systemctl enable nginx
+              sudo systemctl start nginx
+              EOF
+
+key_name = aws_key_pair.nginx-server-ssh.key_name
+}
+
+################# ssh ###################terr
+resource "aws_key_pair" "nginx-server-ssh" {
+  key_name = "nginx-server-ssh"
+  public_key = file("nginx-server.key.pub")
 }
